eaf76c13fe77ab561d6849291755f3c9cd5ae062
Felipe Rios <98775024+FeekDev@users.noreply.github.com>
2026-05-09 11:33:23 -0500

    Remove unused triggers and update trigger comment
    Removed triggers for recalculating sales total and auditing user changes. Updated comments and trigger numbers for clarity.

--- triggers.sql
+++ triggers.sql
@@ -100,38 +100,8 @@ BEGIN
 END;
 GO
 
-/****** Trigger 6: Recalcular total de venta cuando cambia detalle de factura ******/
-CREATE TRIGGER [dbo].[tr_ActualizarTotalVenta]
-ON [dbo].[DetalleFactura]
-AFTER INSERT, UPDATE, DELETE
-AS
-BEGIN
-	SET NOCOUNT ON;
-	
-	DECLARE @id_venta INT;
-	
-	-- Obtener los ID de ventas afectadas (tanto de INSERTED como de DELETED)
-	SELECT DISTINCT @id_venta = [id_venta] FROM INSERTED
-	UNION ALL
-	SELECT DISTINCT [id_venta] FROM DELETED;
-	
-	-- Actualizar el total de las ventas afectadas
-	UPDATE v
-	SET v.[total] = COALESCE((
-		SELECT SUM([subtotal])
-		FROM [dbo].[DetalleFactura]
-		WHERE [id_venta] = v.[id_venta]
-	), 0)
-	FROM [dbo].[Venta] v
-	WHERE v.[id_venta] IN (
-		SELECT DISTINCT [id_venta] FROM INSERTED
-		UNION ALL
-		SELECT DISTINCT [id_venta] FROM DELETED
-	);
-END;
-GO
 
-/****** Trigger 7: Prevenir eliminación de categorías que tienen artículos ******/
+/****** Trigger 6: Prevenir eliminación de categorías que tienen artículos ******/
 CREATE TRIGGER [dbo].[tr_PrevenirEliminarCategoria]
 ON [dbo].[Categoria]
 INSTEAD OF DELETE
@@ -157,7 +127,7 @@ BEGIN
 END;
 GO
 
-/****** Trigger 8: Prevenir eliminación de personas que tienen ventas ******/
+/****** Trigger 7: Prevenir eliminación de personas que tienen ventas ******/
 CREATE TRIGGER [dbo].[tr_PrevenirEliminarPersona]
 ON [dbo].[Persona]
 INSTEAD OF DELETE
@@ -183,81 +153,7 @@ BEGIN
 END;
 GO
 
-/****** Trigger 9: Auditoría de cambios en contraseña de usuarios ******/
--- Primero, crear tabla de auditoría (si no existe)
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'AuditoriaPersona')
-BEGIN
-	CREATE TABLE [dbo].[AuditoriaPersona](
-		[id_auditoria] [int] IDENTITY(1,1) NOT NULL,
-		[id_persona] [int] NOT NULL,
-		[usuario] [nvarchar](50) NOT NULL,
-		[campo_modificado] [nvarchar](50) NOT NULL,
-		[valor_anterior] [nvarchar](255) NULL,
-		[valor_nuevo] [nvarchar](255) NULL,
-		[fecha_cambio] [datetime] NOT NULL DEFAULT GETDATE(),
-		PRIMARY KEY CLUSTERED ([id_auditoria] ASC)
-	);
-END;
-GO
-
-CREATE TRIGGER [dbo].[tr_AuditarCambiosPersona]
-ON [dbo].[Persona]
-AFTER UPDATE
-AS
-BEGIN
-	SET NOCOUNT ON;
-	
-	-- Registrar cambios en contraseña
-	IF UPDATE([contrasena])
-	BEGIN
-		INSERT INTO [dbo].[AuditoriaPersona] ([id_persona], [usuario], [campo_modificado], [valor_anterior], [valor_nuevo], [fecha_cambio])
-		SELECT 
-			i.[id_persona], 
-			i.[usuario], 
-			'contrasena',
-			'***OCULTO***',
-			'***OCULTO***',
-			GETDATE()
-		FROM INSERTED i
-		INNER JOIN DELETED d ON i.[id_persona] = d.[id_persona]
-		WHERE i.[contrasena] <> d.[contrasena];
-	END;
-	
-	-- Registrar cambios en usuario
-	IF UPDATE([usuario])
-	BEGIN
-		INSERT INTO [dbo].[AuditoriaPersona] ([id_persona], [usuario], [campo_modificado], [valor_anterior], [valor_nuevo], [fecha_cambio])
-		SELECT 
-			i.[id_persona], 
-			i.[usuario], 
-			'usuario',
-			d.[usuario],
-			i.[usuario],
-			GETDATE()
-		FROM INSERTED i
-		INNER JOIN DELETED d ON i.[id_persona] = d.[id_persona]
-		WHERE i.[usuario] <> d.[usuario];
-	END;
-	
-	-- Registrar cambios en dirección
-	IF UPDATE([direccion])
-	BEGIN
-		INSERT INTO [dbo].[AuditoriaPersona] ([id_persona], [usuario], [campo_modificado], [valor_anterior], [valor_nuevo], [fecha_cambio])
-		SELECT 
-			i.[id_persona], 
-			i.[usuario], 
-			'direccion',
-			d.[direccion],
-			i.[direccion],
-			GETDATE()
-		FROM INSERTED i
-		INNER JOIN DELETED d ON i.[id_persona] = d.[id_persona]
-		WHERE ISNULL(i.[direccion], '') <> ISNULL(d.[direccion], '');
-	END;
-END;
-GO
-
-/****** Trigger 10: Prevenir stock negativo ******/
+/****** Trigger 8: Prevenir stock negativo ******/
 CREATE TRIGGER [dbo].[tr_PrevenirStockNegativo]
 ON [dbo].[Articulo]
 AFTER UPDATE
